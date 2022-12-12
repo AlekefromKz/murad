b82b97d5cceab328f3a72c9d58c2089dce81c99d
alekefromkz <almaz@thorgate.eu>
2022-12-12 20:22:01 +0200

    deps

---
+++ features/contexts/white_bread_context.exs
@@ -0,0 +1,39 @@
+defmodule WhiteBreadContext do
+  use WhiteBread.Context
+  use Hound.Helpers
+
+  feature_starting_state fn  ->
+    Application.ensure_all_started(:hound)
+    %{}
+  end
+
+  scenario_starting_state fn _state ->
+    Hound.start_session
+    %{}
+  end
+
+  scenario_finalize fn _status, _state ->
+    # Hound.end_session
+    nil
+  end
+
+  given_ ~r/^the following users are existing$/, fn state ->
+    {:ok, state}
+  end
+
+  and_ ~r/^I want to go to the user create page$/, fn state ->
+    {:ok, state}
+  end
+
+  and_ ~r/^I enter the data$/, fn state ->
+    {:ok, state}
+  end
+
+  when_ ~r/^I summit the booking request$/, fn state ->
+    {:ok, state}
+  end
+
+  then_ ~r/^I should receive a confirmation message$/, fn state ->
+    {:ok, state}
+  end
+end
