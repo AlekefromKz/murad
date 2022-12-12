b10d26dcee0a22574f2f638b3597390e421e1198
alekefromkz <almaz@thorgate.eu>
2022-12-12 21:14:35 +0200

    ya ne znayu

--- config/test.exs
+++ config/test.exs
@@ -16,7 +16,7 @@ config :murad, Murad.Repo,
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :murad, MuradWeb.Endpoint,
-  http: [ip: {127, 0, 0, 1}, port: 4002],
+  http: [ip: {127, 0, 0, 1}, port: 4001],
   secret_key_base: "+4lkJWbJACen2k2FkgJzqC8KevoMFyU5ChwjTLGA5xeUJt7yRRqq8y6+OhkdUiq0",
   server: false
 
@@ -28,3 +28,6 @@ config :logger, level: :warn
 
 # Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
+
+config :hound, driver: "chrome_driver"
+config :murad, sql_sandbox: true
--- features/contexts/white_bread_context.exs
+++ features/contexts/white_bread_context.exs
@@ -2,6 +2,7 @@ defmodule WhiteBreadContext do
   use WhiteBread.Context
   use Hound.Helpers
 
+
   feature_starting_state fn  ->
     Application.ensure_all_started(:hound)
     %{}
@@ -13,23 +14,35 @@ defmodule WhiteBreadContext do
   end
 
   scenario_finalize fn _status, _state ->
-    # Hound.end_session
     nil
+    # Hound.end_session
   end
 
-  given_ ~r/^the following users are existing$/, fn state ->
+  given_ ~r/^the following users are existing$/,
+  fn state ->
     {:ok, state}
   end
 
   and_ ~r/^I want to go to the user create page$/, fn state ->
+    navigate_to "/users/new "
+    assert visible_in_page? ~r/New User/
     {:ok, state}
   end
 
-  and_ ~r/^I enter the data$/, fn state ->
-    {:ok, state}
+  and_ ~r/^I enter the data$/, fn state, %{
+    full_name: full_name,
+    email: email,
+    monthly_income: monthly_income,
+    } ->
+
+      fill_field({:id, "full_name"}, full_name)
+      fill_field({:id, "email"}, email)
+      fill_field({:id, "monthly_income"}, monthly_income)
+      {:ok, state}
   end
 
   when_ ~r/^I summit the booking request$/, fn state ->
+    click({:id, "submit_customer"})
     {:ok, state}
   end
 
